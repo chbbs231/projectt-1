faa15226e6f568a440b8a3451c229f5208cae273
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-10-03 21:11:36 -0700

    linked our custom JS to each page and added the Firestore link that was missing. Deleted the firebase analytics link that is not needed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,77 +1,73 @@
 
-  // initializing firebase***
-  const config = {
-    apiKey: 'AIzaSyC1U8sQWVOz6FkYyxtNCBSRl8XDNZY24ao',
-    authDomain: 'projectonebam.firebaseapp.com',
-    databaseURL: 'https://projectonebam.firebaseio.com',
-    projectId: 'projectonebam',
-    storageBucket: '',
-    messagingSenderId: '414001198345',
-    appId: '1:414001198345:web:101af6fb28d538d95e0fd8',
-    measurementId: 'G-MR21YKQQ59'
-  }
-  // Initialize Firebase
-  firebase.initializeApp(config);
+// initializing firebase***
+const config = {
+    apiKey: 'AIzaSyC1U8sQWVOz6FkYyxtNCBSRl8XDNZY24ao',
+    authDomain: 'projectonebam.firebaseapp.com',
+    databaseURL: 'https://projectonebam.firebaseio.com',
+    projectId: 'projectonebam',
+    storageBucket: '',
+    messagingSenderId: '414001198345',
+    appId: '1:414001198345:web:101af6fb28d538d95e0fd8',
+    measurementId: 'G-MR21YKQQ59'
+}
+// Initialize Firebase
+firebase.initializeApp(config);
 
 // incorporating NEWS API ***works with postman***
 // change second line (q=Apple) to say either health, sports, or politics
 
 // to get top news for home page
 var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=152c8213a425472a94f4e747aae707b0';
+    'country=us&' +
+    'apiKey=152c8213a425472a94f4e747aae707b0';
 var req = new Request(url);
 fetch(req)
-    .then(function(response) {
+    .then(function (response) {
         console.log(response.json());
     })
 
 // to get sports articles
 const getSports = sports => {
-var url = 'https://newsapi.org/v2/everything?' +
-          'q=Sports&' +
-          'from=2019-10-04&' +
-          'sortBy=popularity&' +
-          'apiKey=152c8213a425472a94f4e747aae707b0';
+    var url = 'https://newsapi.org/v2/everything?' +
+        'q=Sports&' +
+        'from=2019-10-04&' +
+        'sortBy=popularity&' +
+        'apiKey=152c8213a425472a94f4e747aae707b0';
 
-var req = new Request(url);
+    var req = new Request(url);
 
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
-
-
+    fetch(req)
+        .then(function (response) {
+            console.log(response.json());
+        })
 }
 
 // to get health articles
 const getHealth = health => {
     var url = 'https://newsapi.org/v2/everything?' +
-          'q=Health&' +
-          'from=2019-10-04&' +
-          'sortBy=popularity&' +
-          'apiKey=152c8213a425472a94f4e747aae707b0';
+        'q=Health&' +
+        'from=2019-10-04&' +
+        'sortBy=popularity&' +
+        'apiKey=152c8213a425472a94f4e747aae707b0';
 
-var req = new Request(url);
+    var req = new Request(url);
 
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
-
-
+    fetch(req)
+        .then(function (response) {
+            console.log(response.json());
+        })
 }
 
 // to get politics articles
 const getPolitics = politics => {
     var url = 'https://newsapi.org/v2/everything?' +
-             'q=Politics&' +
-             'from=2019-10-04&' +
-             'sortBy=popularity&' +
-             'apiKey=152c8213a425472a94f4e747aae707b0';
+        'q=Politics&' +
+        'from=2019-10-04&' +
+        'sortBy=popularity&' +
+        'apiKey=152c8213a425472a94f4e747aae707b0';
     var req = new Request(url);
     fetch(req)
-       .then(function(response) {
-           console.log(response.json());
-       })
-    }
+        .then(function (response) {
+            console.log(response.json());
+        })
+}
