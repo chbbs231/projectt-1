8e92f7417f1bbe6adc5f537c581469778bb7cbc2
chbbs231 <atrobles231@gmail.com>
2019-10-07 00:37:16 -0700

    sports.js added

--- assets/javascript/sportapp.js
+++ assets/javascript/sportapp.js
@@ -66,14 +66,14 @@ document.getElementById(`signUp`).addEventListener(`click`, e => {
 
 
 
-/*
+
 // to get top news for home page
 const url = 'https://newsapi.org/v2/everything?q=Sports&from=2019-10-05&sortBy=sports&apiKey=152c8213a425472a94f4e747aae707b0';
 var req = new Request(url);
 fetch(req)
     .then(function (response) {
      console.log (response.json());
-    })*/
+    })
 
 // to get sports articles
 const getSports = sports => {
@@ -81,28 +81,6 @@ const getSports = sports => {
 
     var req = new Request(url);
 
-    fetch(req)
-        .then(function (response) {
-            console.log(response.json());
-        })
-}
-
-// to get health articles
-const getHealth = health => {
-    var url = 'https://newsapi.org/v2/everything?q=Health&from=2019-10-04&sortBy=popularity&apiKey=152c8213a425472a94f4e747aae707b0';
-
-    var req = new Request(url);
-
-    fetch(req)
-        .then(function (response) {
-            console.log(response.json());
-        })
-}
-
-// to get politics articles
-const getPolitics = politics => {
-    var url = 'https://newsapi.org/v2/everything?q=Politics&from=2019-10-04&sortBy=popularity&apiKey=152c8213a425472a94f4e747aae707b0';
-    var req = new Request(url);
     fetch(req)
         .then(function (response) {
             console.log(response.json());
