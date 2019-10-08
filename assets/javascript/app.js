526aefaf344c0fe78fa92c1df135f1a3e0a9731b
chbbs231 <atrobles231@gmail.com>
2019-10-07 21:49:17 -0700

    copied js from master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,17 +60,22 @@ document.getElementById(`signOut`).addEventListener(`click`, e => {
 // change second line (q=Apple) to say either health, sports, or politics
 
 // to get top news for home page
-
-const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=152c8213a425472a94f4e747aae707b0';
+var url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=152c8213a425472a94f4e747aae707b0';
 var req = new Request(url);
 fetch(req)
     .then(function (response) {
-      (response.json());
+        console.log(response.json());
     })
 
 // to get sports articles
 const getSports = sports => {
-    var url = 'https://newsapi.org/v2/everything?q=Sports&from=2019-10-05&sortBy=sports&apiKey=152c8213a425472a94f4e747aae707b0';
+    var url = 'https://newsapi.org/v2/everything?' +
+        'q=Sports&' +
+        'from=2019-10-04&' +
+        'sortBy=popularity&' +
+        'apiKey=152c8213a425472a94f4e747aae707b0';
 
     var req = new Request(url);
 
@@ -82,7 +87,11 @@ const getSports = sports => {
 
 // to get health articles
 const getHealth = health => {
-    var url = 'https://newsapi.org/v2/everything?q=Health&from=2019-10-04&sortBy=popularity&apiKey=152c8213a425472a94f4e747aae707b0';
+    var url = 'https://newsapi.org/v2/everything?' +
+        'q=Health&' +
+        'from=2019-10-04&' +
+        'sortBy=popularity&' +
+        'apiKey=152c8213a425472a94f4e747aae707b0';
 
     var req = new Request(url);
 
@@ -94,42 +103,14 @@ const getHealth = health => {
 
 // to get politics articles
 const getPolitics = politics => {
-    var url = 'https://newsapi.org/v2/everything?q=Politics&from=2019-10-04&sortBy=politics&apiKey=152c8213a425472a94f4e747aae707b0';
+    var url = 'https://newsapi.org/v2/everything?' +
+        'q=Politics&' +
+        'from=2019-10-04&' +
+        'sortBy=popularity&' +
+        'apiKey=152c8213a425472a94f4e747aae707b0';
     var req = new Request(url);
     fetch(req)
         .then(function (response) {
             console.log(response.json());
         })
 }
-
-
-
-const getArticles = article => {
-
-    fetch(`https://newsapi.org/v2/everything?q=${article}&from=2019-10-05&sortBy=popularity&apiKey=152c8213a425472a94f4e747aae707b0`)
-
-        .then(r => r.json())
-        .then(({ articles }) => {
-            // what happens to HTML after search
-            articles.forEach(article => {
-                let articleElem = document.createElement('div')
-                articleElem.innerHTML = `
-                <div class"outer" class="card border-light mb-3">
-                  <div class"inner" class="card-header">${article.title}</div>
-                  <div class="card-body">
-                  <img class="imgcard"src="${article.urlToImage}" class="card-img-top" style="height: 100px" alt="${article.title}"
-                   <span><p> <h5 class="card-title">${article.author}</h5></p></span>
-                    <p class="card-text">${article.content}</p>
-                    <button type="rmbutton" class="btn btn-primary btn-sm">Read more</button>
-                  </div>
-                </div>
-                        `
-                document.getElementById('display').append(articleElem)
-            })
-        })
-}
-getArticles()
-
-
-
-
